f9b16222caf457dddad6f4e00080f528b060d058
Park GyeongHwan <mythos2495@gmail.com>
2025-05-15 18:24:55 +0900

    헤더 fixed 효과 추가

--- src/components/main/header/Header.tsx
+++ src/components/main/header/Header.tsx
@@ -4,11 +4,13 @@ import SearchBar from './SearchBar';
 
 export default function Header() {
   return (
-    <header className="flex flex-1 justify-center">
-      <div className="flex h-[96px] w-full max-w-[1240px] flex-1 items-center justify-between gap-[40px] px-4 py-6 md:justify-start">
-        <Navbar />
-        <SearchBar />
-        <MenuItems />
+    <header className="mb-24 flex w-full flex-1">
+      <div className="fixed top-0 flex w-full justify-center bg-white">
+        <div className="flex h-24 w-full max-w-[1240px] flex-1 items-center justify-between gap-[40px] px-4 py-6 md:justify-start">
+          <Navbar />
+          <SearchBar />
+          <MenuItems />
+        </div>
       </div>
     </header>
   );
