17a9009cf8477c0c8514286dcf34810276fa4161
Park GyeongHwan <mythos2495@gmail.com>
2025-06-12 03:04:46 +0900

    상품 목록 보여주기

--- src/app/(share-header-footer)/category/[category]/page.tsx
+++ src/app/(share-header-footer)/category/[category]/page.tsx
@@ -1,11 +1,13 @@
 import CategoryFilter from '@/components/category/category-filter';
 import CategoryProducts from '@/components/category/category-products';
 
-export default function CatgegoryPage() {
+export default function CatgegoryPage({ params }: { params: { category: string } }) {
   return (
-    <div className="mx-auto flex w-full max-w-[1256px] flex-row border-t border-t-black/10 pt-14 md:pt-17">
-      <CategoryFilter />
-      <CategoryProducts />
+    <div className="mx-auto flex w-full max-w-[1256px] flex-row gap-5 border-t border-t-black/10 pt-14 md:pt-17">
+      <div className="hidden md:block">
+        <CategoryFilter />
+      </div>
+      <CategoryProducts title={decodeURIComponent(params.category)} />
     </div>
   );
 }
--- src/components/category/category-products.tsx
+++ src/components/category/category-products.tsx
@@ -1,3 +1,96 @@
-export default function CategoryProducts() {
-  return <div>Category Products</div>;
+import Product from '../common/product/product';
+
+interface Props {
+  title: string;
+}
+
+const products = [
+  {
+    id: 1,
+    name: 'New Arrival',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+  {
+    id: 2,
+    name: 'New Arrival',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+  {
+    id: 3,
+    name: 'New Arrival',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+  {
+    id: 4,
+    name: 'New Arrival',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+  {
+    id: 5,
+    name: 'Best Seller',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+  {
+    id: 6,
+    name: 'Best Seller',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+  {
+    id: 7,
+    name: 'Best Seller',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+  {
+    id: 8,
+    name: 'Best Seller',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+  {
+    id: 9,
+    name: 'Best Seller',
+    src: '/products/checkered-shirt.png',
+    star: 4.5,
+    price: 100,
+    listPrice: 120,
+  },
+];
+
+export default function CategoryProducts({ title }: Props) {
+  return (
+    <div className="flex flex-1 flex-col gap-4">
+      <div className="flex flex-row justify-between">
+        <div className="text-2xl font-extrabold md:text-3xl">{title}</div>
+      </div>
+      <div className="flex flex-wrap gap-5">
+        {products.map(product => {
+          return <Product key={product.id} product={product} />;
+        })}
+      </div>
+      <div className="border-t border-gray-200 pt-5"></div>
+    </div>
+  );
 }
