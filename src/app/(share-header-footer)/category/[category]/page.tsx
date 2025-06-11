53080eabc42494d7e56c83de4ad122f7e84b4fa4
Park GyeongHwan <mythos2495@gmail.com>
2025-06-12 03:56:45 +0900

    grid 수정
    최대크기 3개에서 줄어들면서 2개로

--- src/app/(share-header-footer)/category/[category]/page.tsx
+++ src/app/(share-header-footer)/category/[category]/page.tsx
@@ -3,11 +3,13 @@ import CategoryProducts from '@/components/category/category-products';
 
 export default function CatgegoryPage({ params }: { params: { category: string } }) {
   return (
-    <div className="mx-auto flex w-full max-w-[1256px] flex-row gap-5 border-t border-t-black/10 pt-14 md:pt-17">
-      <div className="hidden md:block">
+    <div className="mx-auto flex w-full max-w-[1256px] flex-row border-t border-t-black/10 px-2 pt-14 md:pt-17">
+      <div className="hidden rounded-[20px] border border-gray-10 px-4 py-3 py-4 md:block md:px-5">
         <CategoryFilter />
       </div>
-      <CategoryProducts title={decodeURIComponent(params.category)} />
+      <div className="pl-4 md:pl-5">
+        <CategoryProducts title={decodeURIComponent(params.category)} />
+      </div>
     </div>
   );
 }
--- src/components/category/category-products.tsx
+++ src/components/category/category-products.tsx
@@ -1,4 +1,7 @@
+'use client';
+import { useState } from 'react';
 import Product from '../common/product/product';
+import Image from 'next/image';
 
 interface Props {
   title: string;
@@ -80,17 +83,61 @@ const products = [
 ];
 
 export default function CategoryProducts({ title }: Props) {
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <div className="flex flex-1 flex-col gap-4">
       <div className="flex flex-row justify-between">
         <div className="text-2xl font-extrabold md:text-3xl">{title}</div>
       </div>
-      <div className="flex flex-wrap gap-5">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
         {products.map(product => {
           return <Product key={product.id} product={product} />;
         })}
       </div>
-      <div className="border-t border-gray-200 pt-5"></div>
+
+      <div className="flex flex-row justify-between border-t border-gray-200 pt-5">
+        <button className="flex h-9 w-22.5 flex-row items-center justify-center gap-2 rounded-[8px] border border-gray-10 font-medium md:w-27.5">
+          <Image src="/arrow_left.svg" alt="previous icon" width={20} height={20} />
+          Previous
+        </button>
+        <div>
+          <button
+            className={`h-9 w-9 ${currentPage === 1 ? 'bg-[#000]/6' : ''} md:h-10 md:w-10`}
+            onClick={() => setCurrentPage(1)}
+          >
+            1
+          </button>
+          <button
+            className={`h-9 w-9 ${currentPage === 2 ? 'bg-[#000]/6' : ''} md:h-10 md:w-10`}
+            onClick={() => setCurrentPage(2)}
+          >
+            2
+          </button>
+          <button
+            className={`h-9 w-9 ${currentPage === 3 ? 'bg-[#000]/6' : ''} md:h-10 md:w-10`}
+            onClick={() => setCurrentPage(3)}
+          >
+            3
+          </button>
+          <button
+            className={`h-9 w-9 ${currentPage === 4 ? 'bg-[#000]/6' : ''} md:h-10 md:w-10`}
+            onClick={() => setCurrentPage(4)}
+          >
+            4
+          </button>
+          <button
+            className={`h-9 w-9 ${currentPage === 5 ? 'bg-[#000]/6' : ''} md:h-10 md:w-10`}
+            onClick={() => setCurrentPage(5)}
+          >
+            5
+          </button>
+        </div>
+        <button className="flex h-9 w-22.5 flex-row items-center justify-center gap-2 rounded-[8px] border border-gray-10 font-medium md:w-27.5">
+          Next
+          <Image src="/arrow_left.svg" alt="previous icon" width={20} height={20} className="rotate-180" />
+        </button>
+      </div>
     </div>
   );
 }
